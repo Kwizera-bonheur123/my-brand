c935ca8482122a2eea96b56ae2da2ceaa2eb6bb2
kwizera-bonheur123 <kbonheur123@gmail.com>
2024-02-14 15:25:29 +0200

    start work on project section

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("clicked");
-    const icons = document.querySelector(".icons");
-    const navContainer = document.querySelector(".nav-container");
+// Get references to the sidebar and icons
+var sidebarOpen = false;
+var sidebar = document.getElementById("nav-container");
+var icons = document.getElementById("icons");
 
-    icons.addEventListener("click", function () {
-        navContainer.classList.toggle("active");
-    });
+// Function to open the sidebar
+function openSidebar() {
+    console.log("Good");
+    if (!sidebarOpen) {
+        sidebar.classList.add("nav-container-responsive");
+        icons.classList.add("active");
+        sidebarOpen = true;
+    }
+}
+
+// Function to close the sidebar
+function closeSidebar() {
+    if (sidebarOpen) {
+        sidebar.classList.remove("active");
+        icons.classList.remove("active");
+        sidebarOpen = false;
+    }
+}
+
+// Event listeners for clicking on the sidebar toggle icons
+document.getElementById("icons").addEventListener("click", function() {
+    if (sidebarOpen) {
+        closeSidebar();
+    } else {
+        openSidebar();
+    }
 });
