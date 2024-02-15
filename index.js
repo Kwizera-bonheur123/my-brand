08e47d367a975235f41ab22062c4d4ae73b9065d
kwizera-bonheur123 <kbonheur123@gmail.com>
2024-02-15 09:41:46 +0200

    add the responsive to nav bar

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-// Get references to the sidebar and icons
-var sidebarOpen = false;
 var sidebar = document.getElementById("nav-container");
-var icons = document.getElementById("icons");
+var close = document.getElementById("close");
 
-// Function to open the sidebar
 function openSidebar() {
-    console.log("Good");
-    if (!sidebarOpen) {
-        sidebar.classList.add("nav-container-responsive");
-        icons.classList.add("active");
-        sidebarOpen = true;
-    }
-}
+    sidebar.style.display = "flex";
 
-// Function to close the sidebar
+    }
+
 function closeSidebar() {
-    if (sidebarOpen) {
-        sidebar.classList.remove("active");
-        icons.classList.remove("active");
-        sidebarOpen = false;
-    }
+    sidebar.style.display = "none";
 }
-
-// Event listeners for clicking on the sidebar toggle icons
-document.getElementById("icons").addEventListener("click", function() {
-    if (sidebarOpen) {
-        closeSidebar();
-    } else {
-        openSidebar();
-    }
-});
