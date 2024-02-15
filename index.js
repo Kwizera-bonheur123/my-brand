1c2699dfc87612c5dbaf5e0c9138be453653b757
kwizera-bonheur123 <kbonheur123@gmail.com>
2024-02-15 17:21:49 +0200

    finish the responsiveness of landing page

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var sidebar = document.getElementById("nav-container");
 var close = document.getElementById("close");
+var open = document.getElementById("open");
 
 function openSidebar() {
     sidebar.style.display = "flex";
-
+    close.style.display = "flex";
+    open.style.display = "none";
     }
 
 function closeSidebar() {
     sidebar.style.display = "none";
+    close.style.display = "none";
+    open.style.display = "flex";
 }
