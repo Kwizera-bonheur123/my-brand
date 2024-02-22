3cfd9f53b7dd30e8dd789d55a3eb64b48c3d7206
kwizera-bonheur123 <kbonheur123@gmail.com>
2024-02-22 17:18:17 +0200

    add the messages page

--- dashboard.js
+++ dashboard.js
@@ -14,3 +14,31 @@ function closeSidebar() {
         sidebarOpen = false;
     }
 }
+
+
+// When the DOM is fully loaded, execute the script
+
+  // Get the header
+  var header = document.getElementById("header");
+  
+  // Check if the header element exists
+  if (header) {
+    // Get the offset position of the header
+    var sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+      console.log("Scroll position:", window.scrollY);
+      console.log("Sticky position:", sticky);
+      if (window.scrollY >= sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() { myFunction() };
+  } else {
+    console.error("Header element not found!");
+  }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var sidebar = document.getElementById("nav-container");
 var close = document.getElementById("close");
 var open = document.getElementById("open");
 
+//function add the menu icon and remove close icon
+
 function openSidebar() {
     sidebar.style.display = "flex";
     close.style.display = "flex";
@@ -12,4 +14,23 @@ function closeSidebar() {
     sidebar.style.display = "none";
     close.style.display = "none";
     open.style.display = "flex";
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav-bar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    console.log("Scrolled");
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
