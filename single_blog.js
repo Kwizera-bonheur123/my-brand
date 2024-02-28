3ec6bf62e387cdcf336f3b8236cacd130821294d
kwizera-bonheur123 <kbonheur123@gmail.com>
2024-02-28 20:53:59 +0200

    finish to add comment

--- single_blog.js
+++ single_blog.js
@@ -2,7 +2,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 var posts = JSON.parse(localStorage.getItem("Blogs"));
 let single_post = posts.filter((item) => item.id == id);
-console.log(single_post[0].image);
 let signle_blog = document.getElementById("single_blog");
 let newDiv = document.createElement("div");
 newDiv.classList.add("card");
@@ -29,75 +28,47 @@ newDiv.innerHTML = `
             </div>
           </div>
         </div>
-        <h1 class="Recent_comments">50 Recentcomments</h1>
+        <h1 class="Recent_comments">${single_post[0].comments.length} Recentcomments</h1>
         <div class="comment-form">
-          <input type="text" placeholder="Add a comment" />
+          <input id="comment" type="text" placeholder="Add a comment" />
           <div class="comment-buttons">
             <button class="cancel">Cancel</button>
-            <button class="Send">Comment</button>
+            <button onclick="addComment()" class="Send">Comment</button>
           </div>
         </div>
-        <div class="comments">
-          <div class="profile">
-            <img src="./person_1 1.svg" />
-            <div class="comment">
-              <div>
-                <h2 class="writter-name">John Doe</h2>
-                <h2 class="writter-position">OCTOBER 03, 2018 AT 2:21PM</h2>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur quidem laborum necessitatibus, ipsam impedit vitae
-                autem, eum officia, fugiat saepe enim sapiente iste iure! Quam
-                voluptas earum impedit necessitatibus, nihil?
-              </p>
-            </div>
-          </div>
-          <div class="profile">
-            <img src="./person_1 1.svg" />
-            <div class="comment">
-              <div>
-                <h2 class="writter-name">John Doe</h2>
-                <h2 class="writter-position">OCTOBER 03, 2018 AT 2:21PM</h2>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur quidem laborum necessitatibus, ipsam impedit vitae
-                autem, eum officia, fugiat saepe enim sapiente iste iure! Quam
-                voluptas earum impedit necessitatibus, nihil?
-              </p>
-            </div>
-          </div>
-          <div class="profile">
-            <img src="./person_1 1.svg" />
-            <div class="comment">
-              <div>
-                <h2 class="writter-name">John Doe</h2>
-                <h2 class="writter-position">OCTOBER 03, 2018 AT 2:21PM</h2>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur quidem laborum necessitatibus, ipsam impedit vitae
-                autem, eum officia, fugiat saepe enim sapiente iste iure! Quam
-                voluptas earum impedit necessitatibus, nihil?
-              </p>
-            </div>
-          </div>
-          <div class="profile">
-            <img src="./person_1 1.svg" />
-            <div class="comment">
-              <div>
-                <h2 class="writter-name">John Doe</h2>
-                <h2 class="writter-position">OCTOBER 03, 2018 AT 2:21PM</h2>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur quidem laborum necessitatibus, ipsam impedit vitae
-                autem, eum officia, fugiat saepe enim sapiente iste iure! Quam
-                voluptas earum impedit necessitatibus, nihil?
-              </p>
-            </div>
-          </div>
+        <div id="comments" class="comments">
         </div>
 `;
 signle_blog.appendChild(newDiv);
+
+let comment_section = document.getElementById("comments");
+
+for(let i = 0; i < single_post[0].comments.length; i++){
+    let newComment = document.createElement("div");
+    newComment.innerHTML = `
+    <div class="profile">
+            <img src="./person_1 1.svg" />
+            <div class="comment">
+              <div>
+                <h2 class="writter-name">John Doe</h2>
+                <h2 class="writter-position">${single_post[0].comments[i].date}</h2>
+              </div>
+              <p>${single_post[0].comments[i].content}</p>
+            </div>
+          </div>
+    `;
+    comment_section.appendChild(newComment);
+}
+
+function addBlog(posts){
+    localStorage.setItem("Blogs", JSON.stringify(posts));
+  }
+
+let commentInput = document.getElementById("comment");
+function addComment(){
+    let index = posts.findIndex((item) => item.id == id);
+    posts[index].comments.push({content:commentInput.value,date:new Date()});
+    console.log(posts);  
+    addBlog(posts);
+    window.location.reload();
+}
